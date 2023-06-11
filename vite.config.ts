4ef9329d683cff83dfac86d69371a1b326060b2c
jack <jackzhoumine@gmail.com>
2023-06-12 00:11:21 +0800

    修改基础路径

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/*
+ * @Author      : ZhouQiJun
+ * @Date        : 2023-06-11 23:54:36
+ * @LastEditors : ZhouQiJun
+ * @LastEditTime: 2023-06-12 00:09:56
+ * @Description : vite 配置文件
+ */
 import { defineConfig } from 'vite'
+// @ts-ignore
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'web-gis-demos',
 })
